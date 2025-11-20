3f808f438960a5690560ff439df7885babd40197
DeshuiYu-RRC <dyu3@rrc.ca>
2025-11-20 12:59:22 -0600

    Change App test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Provided By Deshui Yu', () => {
   render(<App />);
   const linkElement = screen.getByText(/Provided By Deshui Yu/i);
   expect(linkElement).toBeInTheDocument();
